1cb8e747fc2325929a24d04af81547b403b6a7ae
weibo-5146192729 <matthiaschen@126.com>
2018-12-30 22:35:08 +0800

    backend

--- backend_server/service.py
+++ backend_server/service.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# import pyjsopynrpc
+import pyjsonrpc
 
-# SERVER_HOST = 'localhost'
-# SERVER_PORT = 4040
+SERVER_HOST = 'localhost'
+SERVER_PORT = 4040
 
-# class RequestHandler(pyjsonrpc.HttpRequestHandler):
-#     """Test method"""
-#     @pyjsonrpc.rpcmethod
-#     def add(self, a, b):
-#         print ("add is called")
-#         return a + b
+class RequestHandler(pyjsonrpc.HttpRequestHandler):
+    """Test method"""
+    @pyjsonrpc.rpcmethod
+    def add(self, a, b):
+        print ("add is called")
+        return a + b
 
-# http_server = pyjsonrpc.ThreadingHttpServer(
-#     server_address = (SERVER_HOST, SERVER_PORT),
-#     RequestHandlerClass = RequestHandler
-# )
+http_server = pyjsonrpc.ThreadingHttpServer(
+    server_address = (SERVER_HOST, SERVER_PORT),
+    RequestHandlerClass = RequestHandler
+)
 
-# print ("starting http server on %s:%d" % (SERVER_HOST, SERVER_PORT))
+print ("starting http server on %s:%d" % (SERVER_HOST, SERVER_PORT))
 
-# http_server.serve_forever()
+http_server.serve_forever()
--- frontend/server/rpc_client/rpc_client.js
+++ frontend/server/rpc_client/rpc_client.js
@@ -7,12 +7,14 @@ var client = jayson.client.http({
 
 // test rpc methods
 function add(a, b, callback) {
-    client.request('add', [a, b], function(err, error, response)}{
+    client.request('add', [a, b], function(err, error, response){
         if(err) throw err;
         console.log(response);
         callback(response);
     });
 }
 
-
+module.exports = {
+    add: add
+}
 
